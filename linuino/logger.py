d454690c043d1eeba94579c385041ba9296bfb65
Deshi-basara <simon@i-was-perfect.net>
2015-11-13 19:17:55 +0000

    Basic logger class implemented

---
+++ linuino/logger.py
@@ -0,0 +1,51 @@
+import time
+import datetime
+
+from threading import Thread
+from config import Config
+
+
+class Logger(object):
+    def __init__(self, _postfix, _data_object):
+        """
+        Generates a new file-name for Logger and writes it as JSON-file to
+        the user's file-system.
+        """
+        self.log_file_name = "{0}-{1}.json".format(self.make_stamp(), _postfix)
+        self.log_path = Config.get_log_path()
+        # create log-file
+        try:
+            self.log_file = open(self.log_path + self.log_file_name, "w")
+        except IOError:
+            # @todo error handling
+            print("Path for logfile does not exist: {0}".format(self.log_path))
+
+        # save data-object for pulling new data
+        self.data = _data_object
+
+    def make_stamp(self):
+        """
+        Generates a logging stamp from current data and time.
+        """
+        timestamp = time.time()
+        stamp = datetime.datetime \
+            .fromtimestamp(timestamp) \
+            .strftime(Config.get_log_format())
+        return stamp
+
+    def write_line(self, _line):
+        """
+        Writes a given string as a new line to the Logger's log.
+        """
+        self.log_file.write("{0}\n".format(_line))
+
+    def update(self, json):
+        """
+        Called from the handed data-object.
+        Is used to notify Logger about new data that can be fetched.
+
+        Afterwards Logger requests anounced data from data-object and starts
+        the writing process.
+        """
+        # new_data = self.data.get_new_data()
+        self.write_line(json)
