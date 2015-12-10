aeac875530ceee7149de76f907d4b7a4dad96969
Florian Beck <mail@florianbeck.de>
2015-12-10 12:44:46 +0100

    Added a 2nd version of pidParser.

--- arduino/ArduinOBD/pidparser.ino
+++ arduino/ArduinOBD/pidparser.ino
@@ -1,3 +1,4 @@
+// Version 1 (mit Pointern)
 void parsePID(byte pid, byte data[], String *value, String *unit, String *desc){
   switch (pid) {
     case 0x04: //Calculated engine load value
@@ -28,12 +29,26 @@ void parsePID(byte pid, byte data[], String *value, String *unit, String *desc){
     case 0x0A:
       *value=String(data[0]*3);
       *unit="kPa";
-      *desc="Fuel pressure"
+      *desc="Fuel pressure";
       break;
     case 0x0B:
       *value=String(data[0]);
       *unit="kPa";
       *desc="Intake manifold absolute pressure";
       break;
+    // usw.
+  }
+}
+
+// Version 2 (mit Callbackfunction)
+void parsePIDv2 (byte pid, byte data[], void callback (String value, String unit, String desc)) {
+  switch (pid) {
+    case 0x04:
+      callback(String(data[0]*100/255),"%","Calculated engine load value");break;
+    case 0x05:
+      callback(String(data[0]-40),"°C","Engine coolant temperature");break;
+    case 0x06:
+      callback(String((data[0]-128)*100/128),"%","Long term fuel % trim - Bank 1");break;
+    // usw.
   }
 }
