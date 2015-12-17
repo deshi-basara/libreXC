69a299d207ef387b2b8a45e9443a1cdcba63f79f
Florian Beck <mail@florianbeck.de>
2015-12-17 19:49:25 +0100

    cleaning up and adding comments

--- arduino/ArduinoDebugging/ELM/ELM.ino
+++ arduino/ArduinoDebugging/ELM/ELM.ino
@@ -1,71 +1,81 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial elm(9,10);
 boolean debugging = true; //debugging global on/off
+SoftwareSerial elm(9,10);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  elm.begin(9600);
-  
+  Serial.begin(9600); //(Debugging) Serial USB
+  while (!Serial) {;} // wait for serial port to connect. Needed for native USB port only
+  elm.begin(9600); //Serial ELM  
 }
 
 void loop() {
+  //this is just a test code which asks for temperature every second
   AT(pid(0x04));
+  delay(1000);
 }
 
+/**
+ * pid converts the PID id to the AT command.
+ * 
+ * @param byte id: PID id
+ * @return String ret: response of the ELM327 Shield
+ */
 String pid(byte id) {
   String ret = "01"; //obd mode 1(current data)
   ret += (id<0x10)?"0":""; //leading zero
   ret += String(id,HEX); //pid
   ret.toUpperCase();
-  if(debugging){Serial.println("[Debugging][pid] AT Command for PID "+String(id)+": "+ret);}
+  if(debugging){Serial.println("[Debugging]{pid} AT Command for PID "+String(id)+": "+ret);}
   return ret;
 }
 
-void AT(String Cmd) {
-  //generate command string
-  
+/** 
+ * AT handles the communication with the ELM327 Shield.
+ *  
+ * @param String Cmd: AT Command
+ * @return String Response: response of the ELM327 Shield
+ */
+String AT(String Cmd) {  
   //generate command char array
   int len = Cmd.length();
   char cmd[len+1];
   Cmd.toCharArray(cmd,len+1);
   //send to elm
   elm.println(Cmd);
-  if(debugging){Serial.println("[Debugging][AT] AT Command: "+Cmd);}
-
+  if(debugging){Serial.println("[Debugging]{AT} AT Command: "+Cmd);}
+  //wait for response and process received data
   int i = 0; //received characters  
   unsigned long timestamp = millis(); //set timestamp for timeout
-  String response;
-  while(true) {
+  String Response;
+  while(true) { //listen on serial port until response is complete
     if (elm.available()) {
-      int rec;
+      char response;
       if (i<len) { //echo not complete
-        rec = elm.read();i++;
-        if (cmd[i-1]!=rec){ //check echo characters
-          if(debugging){Serial.println("[Debugging][AT] Zeichen "+String(i-1)+" nicht identisch ("+String(rec)+"). Abbruch.");}
+        response = elm.read();i++;
+        if (cmd[i-1]!=response){ //check echo characters
+          if(debugging){Serial.println("[Debugging][ERROR]{AT} echo not complete");}
           //Todo: error handling
           break;
         }
       } else { //echo complete
-        rec = elm.read();i++;
-        if (rec!=62) {
-          if (rec!=13&&rec!=32) {
-            response += String(rec);
+        response = elm.read();i++;
+        if (response!=62) {
+          if (response!=13/*&&received!=32*/) {
+            Response += String(response);
           }
         } else {
-          if(debugging){Serial.println("[Debugging][AT] Response String: "+response);}
+          if(debugging){Serial.println("[Debugging]{AT} Complete. Response string: "+Response);}
+          return Response;
           break;
         }
       }
     }
     //timeout
     if (millis()>timestamp+2000) {
-      if(debugging){Serial.println("[Debugging][AT] Timeout");}
+      if(debugging){Serial.println("[Debugging]{AT} Timeout");}
       break;
     }
   }
-  if(debugging){Serial.println("[Debugging][AT] Time for this query: "+String(millis()-timestamp))+"ms";}
+  if(debugging){Serial.println("[Debugging]{AT} Response time: "+String(millis()-timestamp))+" ms";}
 }
